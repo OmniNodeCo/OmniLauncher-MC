acfe06186d8272543d458118f0bdc4656fbc7a41
OmniNode <nikolegocabrera@gmail.com>
2026-07-23 17:05:23 -0400

    updated scripts

--- convert_assets.py
+++ convert_assets.py
@@ -1,35 +1,41 @@
 """Convert SVG assets to BMP and ICO for the installer."""
 
-import cairosvg
-from PIL import Image
+import subprocess
 import os
+from PIL import Image
+
+
+INKSCAPE = r"C:\Program Files\Inkscape\bin\inkscape.exe"
+
+
+def svg_to_png(svg_file: str, png_file: str, width: int, height: int) -> None:
+    subprocess.run(
+        [
+            INKSCAPE,
+            svg_file,
+            "--export-type=png",
+            f"--export-filename={png_file}",
+            f"--export-width={width}",
+            f"--export-height={height}",
+        ],
+        check=True,
+    )
+    print(f"Converted {svg_file} to {png_file}")
 
 
 def convert_assets() -> None:
     os.makedirs("assets", exist_ok=True)
 
-    # SVG to PNG
-    cairosvg.svg2png(url="icon.svg", write_to="icon-256.png",
-                     output_width=256, output_height=256)
-    print("Icon SVG to PNG done")
+    svg_to_png("icon.svg", "icon-256.png", 256, 256)
+    svg_to_png("installer-banner.svg", "installer-banner.png", 164, 314)
+    svg_to_png("installer-logo.svg", "installer-logo.png", 55, 55)
 
-    cairosvg.svg2png(url="installer-banner.svg", write_to="installer-banner.png",
-                     output_width=164, output_height=314)
-    print("Banner SVG to PNG done")
-
-    cairosvg.svg2png(url="installer-logo.svg", write_to="installer-logo.png",
-                     output_width=55, output_height=55)
-    print("Logo SVG to PNG done")
-
-    # Banner PNG to BMP
     Image.open("installer-banner.png").save("assets/installer-banner.bmp")
     print("Banner converted to BMP")
 
-    # Logo PNG to BMP
     Image.open("installer-logo.png").save("assets/installer-logo.bmp")
     print("Logo converted to BMP")
 
-    # Icon PNG to ICO
     sizes = [16, 32, 48, 64, 128, 256]
     imgs = []
     for size in sizes:
@@ -45,7 +51,6 @@ def convert_assets() -> None:
         sizes=[(s, s) for s in sizes],
     )
     print("Icon converted to ICO")
-
     print("All assets converted successfully")
 
 
